a9656cfc6ea78155aef9b9e7499af9b6f4d7b170
Bastian Flügel <dev-fluegel@gmx.net>
2024-09-14 09:20:10 +0200

    basic JS

---
+++ script.js
@@ -0,0 +1,27 @@
+function append_operation(operation) {
+    
+    document.getElementById('result_area').innerHTML += operation;
+
+}
+
+function calculate_result() {
+
+    let container = document.getElementById('result_area');
+    let result = eval(container.innerHTML);
+
+    container.innerHTML = result;
+    
+}
+
+function delete_last() {
+
+    let container = document.getElementById('result_area');
+
+    if (container.innerHTML.endsWith(' ')) {
+        container.innerHTML = container.innerHTML.slice(0, -3);
+    } else {
+        container.innerHTML = container.innerHTML.slice(0, -1);
+    }
+    /* code here */
+
+}
