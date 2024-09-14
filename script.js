fa91d28d3b86cb2e16290f7ee07d000267963ed3
Bastian Flügel <dev-fluegel@gmx.net>
2024-09-14 11:18:29 +0200

    css - center box

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+let active_error = false;
+
 function append_operation(operation) {
     
+    if (active_error == true) { return 0; }
+
     document.getElementById('result_area').innerHTML += operation;
+    
 
 }
 
 
 function calculate_result() {
 
+    if (active_error == true) { return 0; }
+
     let container = document.getElementById('result_area');
-    let result = eval(container.innerHTML);
+    let result;
+
+    try {
+        result = eval(container.innerHTML);
+    } catch {
+        result = '#ERROR!';
+        active_error = true;
+        document.getElementById('clear_btn').innerHTML = 'AC';
+    }
 
     container.innerHTML = result;
     
@@ -19,10 +34,20 @@ function delete_last() {
 
     let container = document.getElementById('result_area');
 
-    if (container.innerHTML.endsWith(' ')) {
-        container.innerHTML = container.innerHTML.slice(0, -3);
+    if (active_error == true) {
+
+        container.innerHTML = '';
+        active_error = false;
+        document.getElementById('clear_btn').innerHTML = 'C';
+
     } else {
-        container.innerHTML = container.innerHTML.slice(0, -1);
+
+        if (container.innerHTML.endsWith(' ')) {
+            container.innerHTML = container.innerHTML.slice(0, -3);
+        } else {
+            container.innerHTML = container.innerHTML.slice(0, -1);
+        }
+
     }
 
 }
@@ -31,3 +56,5 @@ function delete_last() {
 function coming_soon() {
     window.alert('coming soon: this function is not avalible now');
 }
+
+
